6b3d1feb8364d6146443c50696a7627709f71018
Max <maximiliano.graf@gmail.com>
2020-11-30 21:48:40 -0300

    Se agregan los datos del alumno GRAF, Maximiliano

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool MaximilianoGraf(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "GRAF",
+        .nombres = "Maximiliano",
+        .documento = "33.717.154",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
 /*=====[Implementations of private functions]================================*/
--- src/main.c
+++ src/main.c
@@ -34,6 +34,17 @@
 
 /*=====[Implementations of private functions]================================*/
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+char cadena[128] = "33.717.154 Graf Maximiliano";
+
+    if (MaximilianoGraf(cadena, sizeof(cadena))) 
+    {
+	printf("Alumno: %s\r\n", cadena);
+    } 
+    else 
+    {
+	printf("No se pudo serializar el alumno\r\n");
+    }
     return 0;
 }
