b04378c920d103296d0d9a6fe49a6ff70af6d329
Joaquin Ulloa <joaquin.g.ulloa@gmail.com>
2020-12-06 17:01:17 -0300

    Se agrega la función del alumno Ulloa

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool JoaquinUlloa(char * cadena, size_t espacio) {
+	const struct alumno_s alumno = {
+		.apellidos = "ULLOA",
+		.nombres = "Joaquín Gaspar",
+		.documento = "36.530.838",
+	};
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
 /*=====[Implementations of private functions]================================*/
