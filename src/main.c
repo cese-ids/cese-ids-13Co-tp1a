ec127287965a4f3f534bf502b069b184266b2900
pablo <pablornar@gmail.com>
2020-12-17 21:12:52 -0300

    modificado de main

--- src/main.c
+++ src/main.c
@@ -62,6 +62,12 @@ int main(int argc, char *argv[]) {
         printf("No se pudo serializar el alumno\r\n");
     }
 
+    if (PabloNarvaez(cadena, sizeof(cadena))) {
+        printf("Alumno: %s\r\n", cadena);
+    } else {
+        printf("No se pudo serializar el alumno\r\n");
+    }
+
     if (MaximilianoGraf(cadena, sizeof(cadena)))  {
 	    printf("Alumno: %s\r\n", cadena);
     } 
